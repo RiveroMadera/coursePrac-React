4e53d6cc5bd46200a59c36934d4fbfc6ed292b7b
RiveroMadera <jesusrivero19@gmail.com>
2021-06-16 00:18:32 -0400

    conecting the Api's information

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
@@ -9,55 +10,54 @@ import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => {
-  const[ videos, steVideos ] = useState([]);
+  const [videos, steVideos] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/initialState')
+      // eslint-disable-next-line arrow-parens
       .then(response => response.json())
+      // eslint-disable-next-line arrow-parens
       .then(data => steVideos(data));
   }, []);
 
-  console.log(videos);
+  /*
+  Async/Await
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/initialState");
+        const data = await response.json();
+        return setVideos(data);
+      } catch {
+        console.log(error);
+      }
+    };
+    fetchVideos();
+  }, []);
+  */
 
   return (
     <div className='App'>
       <Header />
       <Search />
-
-      <Categories title='Mi Lista'>
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      { videos.mylist.length > 0 &&
+        // eslint-disable-next-line react/jsx-wrap-multilines
+        <Categories title='Mi Lista'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories> }
 
       <Categories title='Tendencias'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {videos.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+          ,
         </Carousel>
       </Categories>
 
       <Categories title='Originales de Platzi Video'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
           <CarouselItem />
         </Carousel>
       </Categories>
@@ -65,6 +65,6 @@ const App = () => {
       <Footer />
     </div>
   );
-}
+};
 
 export default App;

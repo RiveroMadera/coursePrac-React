5bc5308356f375f3f732d81a359ae129c5420cdd
RiveroMadera <jesusrivero19@gmail.com>
2021-06-17 14:30:49 -0400

    created component not found

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/styles/components/NotFound.scss';
+
+const NotFound = () => (
+  <section className='error404'>
+    <div className='error404__container'>
+      <h1>Error 404</h1>
+      <h2>Not Found</h2>
+      <img src='' alt='' />
+    </div>
+  </section>
+);
+
+export default NotFound;

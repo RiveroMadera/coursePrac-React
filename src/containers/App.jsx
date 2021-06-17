36b040bdeddcad24e7680f3c77a0477d2a80af71
RiveroMadera <jesusrivero19@gmail.com>
2021-06-16 23:53:54 -0400

    revision

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';

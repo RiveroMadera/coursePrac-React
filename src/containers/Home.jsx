d109a0a03ad411fc45b450a53bbc7e662b9d89d4
RiveroMadera <jesusrivero19@gmail.com>
2021-06-17 20:28:40 -0400

    create actions and reducers folder with index js and changes in home.jsx use redux NOW

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,25 +1,22 @@
+/* eslint-disable semi */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-props-no-spreading */
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
+import { connect } from 'react-redux';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../Hooks/useInitialState';
 
 import '../assets/styles/App.scss';
 
-const API = 'http://localhost:3000/initialState';
-
-const Home = () => {
-  const initialState = useInitialState(API);
-  return videos.length === 0 ? <h1>Loading....</h1> : (
+const Home = ({ myList, trends, originals }) => {
+  return (
     <>
       <Search />
-      {initialState.mylist.length > 0 &&
+      {myList.length > 0 &&
         // eslint-disable-next-line react/jsx-wrap-multilines
         <Categories title='Mi Lista'>
           <Carousel>
@@ -29,14 +26,14 @@ const Home = () => {
 
       <Categories title='Tendencias'>
         <Carousel>
-          {initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+          {trends.map((item) => <CarouselItem key={item.id} {...item} />)}
           ,
         </Carousel>
       </Categories>
 
       <Categories title='Originales de Platzi Video'>
         <Carousel>
-          {initialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
+          {originals.map((item) => <CarouselItem key={item.id} {...item} />)}
           ,
         </Carousel>
       </Categories>
@@ -44,4 +41,13 @@ const Home = () => {
   );
 };
 
-export default Home;
+// eslint-disable-next-line arrow-parens
+const mapsStateToProps = state => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapsStateToProps, null)(Home);

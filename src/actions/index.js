f2dcd5c36f929bff4171d2417ccb643207cc0f5a
RiveroMadera <jesusrivero19@gmail.com>
2021-06-17 21:06:18 -0400

    add function set favorite

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable arrow-parens */
+/* eslint-disable no-multi-assign */
+/* eslint-disable import/prefer-default-export */
+export const setFavorite = payload => ({
+  type: 'SET_FAVORITE',
+  payload,
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,13 @@
-const reducer = (state) => {
-  return state;
+/* eslint-disable semi */
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_FAVORITE':
+      return {
+        ...state,
+        myList: [...state.myList, action.payload],
+      }
+    default: state;
+  }
 };
 
 export default reducer;

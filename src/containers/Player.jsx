fadc1468c0a67b66b07c213f7c7a41d8417d79b9
RiveroMadera <jesusrivero19@gmail.com>
2021-06-19 01:56:09 -0400

    work details for player

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
-
+/* eslint-disable react/destructuring-assignment */
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
 import '../assets/styles/components/Player.scss';
+import NotFound from './NotFound';
 
-const Player = () => {
-  return (
+const Player = (props) => {
+  const { id } = props.match.params;
+  const hasPLaying = Object.keys(props.playing).length > 0;
+
+  useEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+
+  return hasPLaying ? (
     <>
       <div className='Player'>
         <video controls autoPlay>
-          <source src='' type='video/mp4' />
+          <source src={props.playing.source} type='video/mp4' />
         </video>
       </div>
       <div className='Player-back'>
-        <button type='button'>
+        <button type='button' onClick={() => props.history.goBack()}>
           Regresar
         </button>
       </div>
     </>
-  );
+  ) : <NotFound />;
 };
 
-export default Player;
+const mapStateToProps = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
+
+const mapDispatchToProps = {
+  getVideoSource,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);

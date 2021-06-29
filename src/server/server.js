a1ddfb1dd6a8ce009f9d6bccb93171edc1590878
RiveroMadera <jesusrivero19@gmail.com>
2021-06-29 14:56:04 -0400

    config with nodemon on server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const { ENV, PORT } = process.env;
 const app = express();
+
+if (ENV === 'development') {
+  console.log('development config');
+}
 
 app.get('*', (req, res) => {
   res.send({ hello: 'express' });
 });
 
-app.listen(3000, (err) => {
-  if (err) { console.log(err); } else { console.log('server running on 3000 port'); }
+app.listen(PORT, (err) => {
+  if (err) { console.log(err); } else { console.log(`server running on port ${PORT}`); }
 });

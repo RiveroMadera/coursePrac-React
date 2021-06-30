d5562f091e05dce7466fcb1602f44675da5d5b99
RiveroMadera <jesusrivero19@gmail.com>
2021-06-30 01:55:00 -0400

    change format

--- src/frontend/containers/Home.jsx
+++ src/frontend/containers/Home.jsx
@@ -21,9 +21,9 @@ const Home = ({ myList, trends, originals }) => {
       <Header />
         <Search isHome />
           {myList.length > 0 && (
-            <Categories title='mi list'>
+            <Categories title='mi Lista'>
               <Carousel>
-                {trends.map((item) => (
+                {myList.map((item) => (
                   <CarouselItem
                     key={item.id}
                     {...item}
@@ -33,18 +33,14 @@ const Home = ({ myList, trends, originals }) => {
               </Carousel>
             </Categories>
           )}
-
       <Categories title='Tendencias'>
         <Carousel>
           {trends.map((item) => <CarouselItem key={item.id} {...item} />)}
-          ,
         </Carousel>
       </Categories>
-
       <Categories title='Originales de Platzi Video'>
         <Carousel>
           {originals.map((item) => <CarouselItem key={item.id} {...item} />)}
-          ,
         </Carousel>
       </Categories>
     </>

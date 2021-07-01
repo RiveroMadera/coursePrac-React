c7345b9756a0dc4de767277bb172370e4d42ff6e
RiveroMadera <jesusrivero19@gmail.com>
2021-06-30 22:55:52 -0400

    config server to production

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
 import React from 'react';
+import helmet from 'helmet';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
@@ -33,6 +34,11 @@ if (ENV === 'development') {
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
   // app.use(require("webpack-hot-middleware")(compiler));
+} else {
+  app.use(express.static(`${__dirname}/public`));
+  app.use(helmet());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.disable('x-powered-by');
 }
 
 const setResponse = (html) => {
